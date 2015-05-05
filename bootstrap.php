21896b7eef854e03479a2511167db029b42ecffa
anolilab <info@anolilab.de>
2015-05-05 16:23:45 +0200

    remove not needed files, change on bootstrap phpnuit file

--- bootstrap.php
+++ bootstrap.php
@@ -7,13 +7,7 @@
 
 define('PHPUNIT_DB_PREFIX', 'phpunit_');
 
-global $wp_rewrite, $wpdb;
-
 //Required for code coverage to run.
 //define( 'WP_MAX_MEMORY_LIMIT', '1024M' );
 define('WP_MEMORY_LIMIT', '100M');
 
-require_once(dirname( __FILE__ ) . '/web/wp/wp-load.php');
-require_once(ABSPATH . 'web/wp/wp-admin/includes/admin.php');
-
-wp_set_current_user(1);
