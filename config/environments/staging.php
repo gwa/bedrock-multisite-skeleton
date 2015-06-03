10c6451b0df2d15fe19445d323d79b117a0dd223
anolilab <info@anolilab.de>
2015-06-03 13:25:58 +0200

    cs fixes

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -4,4 +4,3 @@ ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
-
